60ddb4f7db81277a9d7246008c763c3a246404cd
Nouhaila <92635268+Nouhaila200@users.noreply.github.com>
2025-08-15 21:10:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def load_css():
         """,
         unsafe_allow_html=True
     )
-     with open("style.css") as f:
+    with open("style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 load_css()

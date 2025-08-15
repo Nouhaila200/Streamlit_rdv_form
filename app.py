2cad3991d3c369143ca3553cfcc848efe64ab074
Nouhaila <92635268+Nouhaila200@users.noreply.github.com>
2025-08-15 20:56:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def load_css():
         """
         <style>
         .stApp {
-            background-image: url("https://raw.githubusercontent.com/Nouhaila200/Streamlit_rdv_form/main/background.jpg");
+            background-image: url("https://raw.githubusercontent.com/Nouhaila200/Streamlit_rdv_form/main/ChatGPT Image 15 août 2025, 20_21_43.png");
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
